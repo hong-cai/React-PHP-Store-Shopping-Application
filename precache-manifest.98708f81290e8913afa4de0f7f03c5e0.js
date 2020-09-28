80a003d81f8675bdbf8e96a47d43441e24e1ca37
Hong Cai <hongcai.nelson@gmail.com>
2020-09-28 15:18:33 +1300

    Updates

---
+++ precache-manifest.98708f81290e8913afa4de0f7f03c5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe1f187fd56d222a4be3cd463da21ba",
+    "url": "/React-Store-Shopping-Application/index.html"
+  },
+  {
+    "revision": "38a9b180239d1e13fd1f",
+    "url": "/React-Store-Shopping-Application/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "2fb994b052b017608798",
+    "url": "/React-Store-Shopping-Application/static/css/main.7fa51ee5.chunk.css"
+  },
+  {
+    "revision": "38a9b180239d1e13fd1f",
+    "url": "/React-Store-Shopping-Application/static/js/2.8e93801b.chunk.js"
+  },
+  {
+    "revision": "2fb994b052b017608798",
+    "url": "/React-Store-Shopping-Application/static/js/main.66a18403.chunk.js"
+  },
+  {
+    "revision": "3bd00e639efd6842f135",
+    "url": "/React-Store-Shopping-Application/static/js/runtime~main.e83e1fa3.js"
+  },
+  {
+    "revision": "77b5ca7fe599d99607bf15022a3deaba",
+    "url": "/React-Store-Shopping-Application/static/media/bg-spittle-texture.77b5ca7f.jpg"
+  }
+]);
--- src/components/Textblock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default ({ content }) => {
-    return (
-
-        <div className="row"> <div className="col-8 mx-auto"><h5 className="text-center">{content}</h5></div>  </div>
-
-    )
-}
